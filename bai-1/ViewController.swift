03b1bf204462d1096ee4fe7dedf33603182c2ffd
Khoa Dong <khoadong@Khoas-MacBook-Pro.local>
2016-05-26 20:42:10 +0700

    Update new files

--- bai-1/ViewController.swift
+++ bai-1/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    
+    @IBAction func btn1a(sender: AnyObject) {
+        lbl.text = ""
+        lbl.text = "HELLO WORLD"
+    }
+    @IBAction func btn1b(sender: AnyObject) {
+        lbl.text = ""
+        let txt = NSMutableAttributedString.init(string: "HELLO WORLD")
+        
+        txt.setAttributes([NSFontAttributeName: UIFont.systemFontOfSize(20), NSForegroundColorAttributeName: UIColor.redColor()], range: NSMakeRange(0,5))// bắt đầu từ vị trí số 0, kéo dài 5 kí tự
+        
+        txt.setAttributes([NSFontAttributeName: UIFont.systemFontOfSize(10), NSForegroundColorAttributeName: UIColor.blueColor()], range: NSMakeRange(6, 5))// tương tự
+        lbl.attributedText = txt
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
